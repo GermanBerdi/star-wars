062b18f4cb9b5776392e28dd9f4b663b18d4f36f
German Berdichevsky <gberdichevsky@naevatec.com>
2025-06-11 20:58:02 +0200

    feat: [master] initialize API with Express and TypeScript

---
+++ src/api/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('¡Hola desde Express con TypeScript!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
+
