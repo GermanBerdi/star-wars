79f400a6877b39aaa8b688ae0aed54ea72f52c77
German Berdichevsky <gberdichevsky@naevatec.com>
2025-06-15 12:26:14 +0200

    fix: [main] service uses new repo export format, fixes some function names

--- src/services/fights/fights-service.ts
+++ src/services/fights/fights-service.ts
@@ -1,20 +1,20 @@
-import { getCharacterById } from "../../db/characters/characters-repo";
-import { INewFight, IFightRow } from "../../db/fights/fights-interfaces";
-import { createFight, getAllFights } from "../../db/fights/fights-repo";
+import characterRepo from "../../db/characters/characters-repo";
+import { INewFightReq, IFightRow } from "./fights-interfaces";
+import fightRepo from "../../db/fights/fights-repo";
 
 const create = async (id1: number, id2: number): Promise<IFightRow> => {
   try {
-    const character1 = await getCharacterById(id1);
+    const character1 = await characterRepo.getById(id1);
     if (!character1) throw new Error(`Character with id ${id1} not found.`);
-    const character2 = await getCharacterById(id2);
+    const character2 = await characterRepo.getById(id2);
     if (!character2) throw new Error(`Character with id ${id2} not found.`);
-    const newFight: INewFight = {
+    const newFight: INewFightReq = {
       character1_id: character1.id,
       character2_id: character2.id,
       character1_current_hp: character1.hp,
       character2_current_hp: character2.hp,
     };
-    const fight = await createFight(newFight);
+    const fight = await fightRepo.create(newFight);
     return fight;
   } catch (error) {
     const errorMessage = `Error in create fight service: ${error}`;
@@ -23,12 +23,12 @@ const create = async (id1: number, id2: number): Promise<IFightRow> => {
   }
 };
 
-const listAll = async () => {
+const getAll = async () => {
   try {
-    const fights = await getAllFights();
+    const fights = await fightRepo.getAll();
     return fights;
   } catch (error) {
-    const errorMessage = `Error in listAll fight service: ${error}`;
+    const errorMessage = `Error in getAll fight service: ${error}`;
     console.error(errorMessage);
     throw new Error(errorMessage);
   }
@@ -36,7 +36,7 @@ const listAll = async () => {
 
 const service = {
   create,
-  listAll,
+  getAll,
 };
 
 export default service;
