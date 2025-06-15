de8153c1ecc2a4c1665f8b9392dfeffe9f35780e
German Berdichevsky <gberdichevsky@naevatec.com>
2025-06-15 20:07:01 +0200

    fix: [main] properly terminate each SQL statement

--- src/db/characters/characters-repo.ts
+++ src/db/characters/characters-repo.ts
@@ -1,11 +1,11 @@
 import { ResultSetHeader } from "mysql2";
 import pool from "../connection";
-import { INewCharacterReq, IUpdateCharacterReq, ICharacterRow } from "../../services/characters/characters-interfaces";
+import { INewCharacterReq, ICharacterRow, IUpdateCharacterReq } from "../../services/characters/characters-interfaces";
 
 const create = async (newCharacter: INewCharacterReq): Promise<ICharacterRow> => {
   const { name, hp, strength, defense, speed } = newCharacter;
   const [result] = await pool.execute<ResultSetHeader>(
-    `INSERT INTO characters (name, hp, strength, defense, speed) VALUES (?, ?, ?, ?, ?)`,
+    `INSERT INTO characters (name, hp, strength, defense, speed) VALUES (?, ?, ?, ?, ?);`,
     [name, hp, strength, defense, speed],
   );
   if (result.affectedRows !== 1) throw new Error(JSON.stringify(result));
@@ -20,19 +20,19 @@ const update = async (characterToUpdate: IUpdateCharacterReq): Promise<ICharacte
   const setClause = keys.map((key) => `${key} = ?`).join(", ");
   const values = keys.map((key) => fields[key as keyof typeof fields]);
   values.push(id);
-  const [result] = await pool.execute<ResultSetHeader>(`UPDATE characters SET ${setClause} WHERE id = ?`, values);
+  const [result] = await pool.execute<ResultSetHeader>(`UPDATE characters SET ${setClause} WHERE id = ?;`, values);
   if (result.affectedRows !== 1) throw new Error(JSON.stringify(result));
-  const [row] = await pool.execute<ICharacterRow[]>(`SELECT * FROM characters WHERE id = ?`, [id]);
+  const [row] = await pool.execute<ICharacterRow[]>(`SELECT * FROM characters WHERE id = ?;`, [id]);
   return row[0];
 };
 
 const getAll = async (): Promise<ICharacterRow[]> => {
-  const [rows] = await pool.query<ICharacterRow[]>("SELECT * FROM characters");
+  const [rows] = await pool.query<ICharacterRow[]>("SELECT * FROM characters;");
   return rows;
 };
 
 const getById = async (id: number): Promise<ICharacterRow | null> => {
-  const [rows] = await pool.query<ICharacterRow[]>(`SELECT * FROM characters WHERE id = ?`, [id]);
+  const [rows] = await pool.query<ICharacterRow[]>(`SELECT * FROM characters WHERE id = ?;`, [id]);
   return rows.length > 0 ? rows[0] : null;
 };
 
