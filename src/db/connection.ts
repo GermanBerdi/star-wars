3d0f1182cf4117cea81d15b71a83620b4e912d1b
German Berdichevsky <gberdichevsky@naevatec.com>
2025-06-16 10:44:50 +0200

    chore: [main] switch to host.docker.internal for WSL2 compatibility

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,7 +1,7 @@
 import mysql, { Pool } from "mysql2/promise";
 
 const pool: Pool = mysql.createPool({
-  host: process.env.DB_HOST || "192.168.10.235",
+  host: process.env.DB_HOST || "host.docker.internal",
   port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "M@sterkey1",
