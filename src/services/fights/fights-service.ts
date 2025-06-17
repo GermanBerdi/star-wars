d0b8599c1dfc1fd03d84d3ad3fe4691c9a59d668
German Berdichevsky <gberdichevsky@naevatec.com>
2025-06-17 15:50:34 +0200

    feat: [main] refactor create and getById feat: [main] use repository method getByIdPopulated in service feat: [main] implement applyActionEffects to reduce database access

--- src/services/fights/fights-service.ts
+++ src/services/fights/fights-service.ts
@@ -1,19 +1,22 @@
-import characterService from "../characters/characters-service";
-import { IFightRow, INewFightReq, IGetByIddPopulatedRes, IUpdateFightReq } from "./fights-interfaces";
 import fightRepo from "../../db/fights/fights-repo";
+
+import characterService from "../characters/characters-service";
+import { IFightRow, INewFightReq, IFightPopulatedRow, IUpdateFightReq } from "./fights-interfaces";
 import { WinnerId } from "./fights-enums";
 
 const create = async (id1: number, id2: number): Promise<IFightRow> => {
   try {
-    const combatant1 = await characterService.getById(id1);
-    if (!combatant1) throw new Error(`Character with id ${id1} not found.`);
-    const combatant2 = await characterService.getById(id2);
-    if (!combatant2) throw new Error(`Character with id ${id2} not found.`);
+    const character1 = await characterService.getById(id1);
+    const character2 = await characterService.getById(id2);
     const newFight: INewFightReq = {
-      combatant1Id: combatant1.id,
-      combatant2Id: combatant2.id,
-      combatant1Hp: combatant1.hp,
-      combatant2Hp: combatant2.hp,
+      combatant1: {
+        id: character1.id,
+        hp: character1.hp,
+      },
+      combatant2: {
+        id: character2.id,
+        hp: character2.hp,
+      },
     };
     const fight = await fightRepo.create(newFight);
     return fight;
@@ -35,9 +38,10 @@ const getAll = async (): Promise<IFightRow[]> => {
   }
 };
 
-const getById = async (id: number): Promise<IFightRow | null> => {
+const getById = async (id: number): Promise<IFightRow> => {
   try {
     const fight = await fightRepo.getById(id);
+    if (!fight) throw new Error(`Fight with id ${id} not found.`);
     return fight;
   } catch (error) {
     const errorMessage = `Error in getById at fight service: ${error}`;
@@ -46,53 +50,10 @@ const getById = async (id: number): Promise<IFightRow | null> => {
   }
 };
 
-const getByIdPopulated = async (id: number): Promise<IGetByIddPopulatedRes> => {
+const getByIdPopulated = async (id: number): Promise<IFightPopulatedRow> => {
   try {
-    const fight = await getById(id);
-    if (!fight) {
-      const errorMessage = `Error in getByIdPopulated at fight service: Fight not found`;
-      console.error(errorMessage);
-      throw new Error(errorMessage);
-    }
-    const character1 = await characterService.getById(fight.combatant1_id);
-    if (!character1) {
-      const errorMessage = `Error in getByIdPopulated at fight service: Not character was found with character id ${fight.combatant1_id} at fight ${id}`;
-      console.error(errorMessage);
-      throw new Error(errorMessage);
-    }
-    const character2 = await characterService.getById(fight.combatant2_id);
-    if (!character2) {
-      const errorMessage = `Error in getByIdPopulated at fight service: Not character was found with character id ${fight.combatant2_id} at fight ${id}`;
-      console.error(errorMessage);
-      throw new Error(errorMessage);
-    }
-    const fightPopulated: IGetByIddPopulatedRes = {
-      id: fight.id,
-      combatant1: {
-        id: 1,
-        characterId: character1.id,
-        name: character1.name,
-        hp: fight.combatant1_hp,
-        maxHp: character1.hp,
-        strength: character1.strength,
-        defense: character1.defense,
-        speed: character1.speed,
-      },
-      combatant2: {
-        id: 2,
-        characterId: character2.id,
-        name: character2.name,
-        hp: fight.combatant2_hp,
-        maxHp: character2.hp,
-        strength: character2.strength,
-        defense: character2.defense,
-        speed: character2.speed,
-      },
-      turn: fight.turn,
-      winner_id: fight.winner_id,
-      updated_at: fight.updated_at,
-      created_at: fight.created_at,
-    };
+    const fightPopulated = await fightRepo.getByIdPopulated(id);
+    if (!fightPopulated) throw new Error(`Fight with id ${id} not found.`);
     return fightPopulated;
   } catch (error) {
     const errorMessage = `Error in getByIdPopulated at fight service: ${error}`;
@@ -101,26 +62,8 @@ const getByIdPopulated = async (id: number): Promise<IGetByIddPopulatedRes> => {
   }
 };
 
-const setCombatantHp = (fight: IUpdateFightReq, targetCombatant: number, hp: number): IUpdateFightReq => {
-  const newFightData = fight;
-  if (targetCombatant === 1) newFightData.combatant1_hp = hp;
-  else newFightData.combatant2_hp = hp;
-  return newFightData;
-};
-
-const updateCombatantHp = async (fightId: number, targetCombatant: number, updatedHp: number): Promise<IFightRow> => {
-  try {
-    const fight: IUpdateFightReq = {
-      id: fightId,
-    };
-    const fightToUpdate = setCombatantHp(fight, targetCombatant, updatedHp);
-    const fightUpdated = await fightRepo.update(fightToUpdate);
-    return fightUpdated;
-  } catch (error) {
-    const errorMessage = `Error in updateCombatantHp at fight service: ${error}`;
-    console.error(errorMessage);
-    throw new Error(errorMessage);
-  }
+const isFinished = (winnerId: WinnerId): boolean => {
+  return winnerId !== WinnerId.NoWinner;
 };
 
 const calculateWinnerId = (fight: IFightRow): WinnerId => {
@@ -130,24 +73,32 @@ const calculateWinnerId = (fight: IFightRow): WinnerId => {
   return WinnerId.NoWinner;
 };
 
-const isFinished = (winnerId: WinnerId): boolean => {
-  return winnerId !== WinnerId.NoWinner;
-};
-
-const updateIfFinished = async (fight: IFightRow): Promise<IFightRow> => {
+const applyActionEffects = async (
+  fight: IFightRow,
+  targetCombatantId: number,
+  targetUpdatedHp: number,
+): Promise<IFightRow> => {
   try {
-    const newWinnerId = calculateWinnerId(fight);
-    if (fight.winner_id !== newWinnerId) {
-      const fightToUpdate: IUpdateFightReq = {
-        id: fight.id,
-        winner_id: newWinnerId,
-      };
-      const fightUpdated = await fightRepo.update(fightToUpdate);
-      return fightUpdated;
+    const newFightState = { ...fight };
+    const fightToUpdate: IUpdateFightReq = {
+      id: fight.id,
+    };
+    if (targetCombatantId === 1) {
+      newFightState.combatant1_hp = targetUpdatedHp;
+      fightToUpdate.combatant1_hp = targetUpdatedHp;
+    } else {
+      newFightState.combatant2_hp = targetUpdatedHp;
+      fightToUpdate.combatant2_hp = targetUpdatedHp;
     }
-    return fight;
+    const newWinnerId = calculateWinnerId(newFightState);
+    if (newFightState.winner_id !== newWinnerId) {
+      newFightState.winner_id = newWinnerId;
+      fightToUpdate.winner_id = newWinnerId;
+    }
+    const fightUpdated = await fightRepo.update(fightToUpdate);
+    return fightUpdated;
   } catch (error) {
-    const errorMessage = `Error in updateIfFinished at fight service: ${error}`;
+    const errorMessage = `Error in applyActionEffects at fight service: ${error}`;
     console.error(errorMessage);
     throw new Error(errorMessage);
   }
@@ -158,9 +109,8 @@ const service = {
   getAll,
   getById,
   getByIdPopulated,
-  updateCombatantHp,
   isFinished,
-  updateIfFinished,
+  applyActionEffects,
 };
 
 export default service;
