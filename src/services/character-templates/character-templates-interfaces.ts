e341df9fd843ec4029640c5f15e6cf555a0ec081
German Berdichevsky <gberdichevsky@naevatec.com>
2025-06-22 14:03:43 +0200

    feat: [SWCS-1] adapt character-template interfaces and enums to new design

--- src/services/character-templates/character-templates-interfaces.ts
+++ src/services/character-templates/character-templates-interfaces.ts
@@ -1,27 +1,35 @@
+import { CharacterType } from "./character-templates-enums";
+
 export interface ICharacterTemplateRow {
   id: number;
   character_name: string;
-  hp: number;
   strength: number;
   defense: number;
   speed: number;
+  hp: number;
+  character_type: CharacterType;
+  character_description: string;
   updated_at: Date;
   created_at: Date;
 }
 
 export interface INewCharacterTemplateReq {
   character_name: string;
-  hp: number;
   strength: number;
   defense: number;
   speed: number;
+  hp: number;
+  character_type: CharacterType;
+  character_description: string | null;
 }
 
 export interface IUpdateCharacterTemplateReq {
   id: number;
   character_name?: string;
-  hp?: number;
   strength?: number;
   defense?: number;
   speed?: number;
+  hp?: number;
+  character_type?: CharacterType;
+  character_description?: string;
 }
